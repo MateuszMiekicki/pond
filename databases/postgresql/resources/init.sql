5ad4375a5ef96e7cfb74f21f5d375a3a53aa5b9d
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-06-29 18:37:34 +0200

    fix problems with db and smtp server

--- databases/postgresql/resources/init.sql
+++ databases/postgresql/resources/init.sql
@@ -32,36 +32,45 @@ CREATE TABLE sensor (
     CONSTRAINT device_fk FOREIGN KEY (device_id) REFERENCES device (id),
     CONSTRAINT constraint_device_pin_number UNIQUE (device_id, pin_number)
 );
-CREATE TABLE visitor { id SERIAL,
-user_id SERIAL,
-device_id SERIAL,
-name VARCHAR NOT NULL,
-PRIMARY KEY (id),
-CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES "user" (id),
-CONSTRAINT device_fk FOREIGN KEY (device_id) REFERENCES device (id) };
-CREATE TABLE alert { id SERIAL,
-device_id SERIAL,
-sensor_id SERIAL,
-date TIMESTAMP NOT NULL,
-description VARCHAR NOT NULL,
-served BOOLEAN NOT NULL,
-PRIMARY KEY (id),
-CONSTRAINT device_fk FOREIGN KEY (device_id) REFERENCES device (id),
-CONSTRAINT sensor_fk FOREIGN KEY (sensor_id) REFERENCES sensor (id),
-CONSTRAINT visitor_fk FOREIGN KEY (visitor_id) REFERENCES visitor (id) };
-CREATE TABLE general_alert { id SERIAL,
-device_id SERIAL,
-date TIMESTAMP NOT NULL,
-description VARCHAR NOT NULL,
-served BOOLEAN NOT NULL,
-PRIMARY KEY (id),
-CONSTRAINT device_fk FOREIGN KEY (device_id) REFERENCES device (id) };
-CREATE TABLE pet { id SERIAL,
-name VARCHAR NOT NULL,
-description VARCHAR,
-PRIMARY KEY (id) };
-CREATE TABLE pet_habitat { id SERIAL,
-pet_id SERIAL,
-PRIMARY KEY (id),
-CONSTRAINT pet_fk FOREIGN KEY (pet_id) REFERENCES pet (id),
-CONSTRAINT sensor_fk FOREIGN KEY (sensor_id) REFERENCES sensor (id) };
+CREATE TABLE visitor (
+    id SERIAL,
+    user_id SERIAL,
+    device_id SERIAL,
+    name VARCHAR NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES "user" (id),
+    CONSTRAINT device_fk FOREIGN KEY (device_id) REFERENCES device (id)
+);
+CREATE TABLE alert (
+    id SERIAL,
+    device_id SERIAL,
+    sensor_id SERIAL,
+    date TIMESTAMP NOT NULL,
+    description VARCHAR NOT NULL,
+    served BOOLEAN NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT device_fk FOREIGN KEY (device_id) REFERENCES device (id),
+    CONSTRAINT sensor_fk FOREIGN KEY (sensor_id) REFERENCES sensor (id),
+);
+CREATE TABLE general_alert (
+    id SERIAL,
+    device_id SERIAL,
+    date TIMESTAMP NOT NULL,
+    description VARCHAR NOT NULL,
+    served BOOLEAN NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT device_fk FOREIGN KEY (device_id) REFERENCES device (id)
+);
+CREATE TABLE pet (
+    id SERIAL,
+    name VARCHAR NOT NULL,
+    description VARCHAR,
+    PRIMARY KEY (id)
+);
+CREATE TABLE pet_habitat (
+    id SERIAL,
+    pet_id SERIAL,
+    PRIMARY KEY (id),
+    CONSTRAINT pet_fk FOREIGN KEY (pet_id) REFERENCES pet (id),
+    CONSTRAINT sensor_fk FOREIGN KEY (sensor_id) REFERENCES sensor (id)
+);
